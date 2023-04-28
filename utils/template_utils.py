9fc2120b31e8ffa9fa474d0ad9f12515974a6f73
ROIM1998 <bowen98@uw.edu>
2023-04-28 23:19:37 +0000

    Add simple PromptBuilder supporting zero-shot and few-shot settings

--- utils/template_utils.py
+++ utils/template_utils.py
@@ -1,6 +1,8 @@
 import re
 import traceback
 
+from typing import Optional, Dict, List, Union
+
 def parse_bracket_template(s, params):
     if not isinstance(s, str):
         return s
@@ -13,3 +15,29 @@ def parse_bracket_template(s, params):
         print('Template: {}'.format(s))
         traceback.print_exc()
         return None
+    
+class PromptBuilder:
+    def __init__(self, template:Optional[str]=None):
+        self.template = template
+
+    def build_prompt(self, params: Union[Dict[str, str], List[Dict[str, str]]], setting: str = 'zero-shot'):
+        if self.template is None:
+            print('No template found, returning params by simple key-value concatenation')
+            return ' '.join('{}: {}'.format(k, v) for k, v in params.items())
+        if setting == 'zero-shot':
+            assert isinstance(params, dict)
+            return parse_bracket_template(self.template, params)
+        elif setting == 'few-shot':
+            assert isinstance(params, list)
+            # Simply treating the first example as the query, and the rest as the support set
+            query, supports = params[0], params[1:]
+            params = query
+            if len(supports) == 1:
+                for k, v in supports[0].items():
+                    params['ex_{}'.format(k)] = v
+            else:
+                for i, support in enumerate(supports):
+                    for k, v in support.items():
+                        params['ex{}_{}'.format(i, k)] = v
+        return parse_bracket_template(self.template, params)
+        
